19133b815819b4f43ba77ce0823052a24c5f0629
serafim gray <serafim619@gmail.com>
2021-05-05 11:22:56 +0300

    imported assets

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,11 @@ const CONFIG = {
           {
             from: "src/video/",
             to: "video/",
-          }
+          },
+          {
+            from: "src/assets/",
+            to: "",
+          },
         ]
       }
     ),
