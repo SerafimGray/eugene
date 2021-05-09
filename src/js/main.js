580ff9ed3aec76276e11c53f9027a0783eaef592
serafim gray <serafim619@gmail.com>
2021-05-09 06:41:18 +0300

    assets optimization

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,24 @@
+import { throttle } from 'lodash';
 import "../css/normalize.css";
 import "../css/_main.css";
+
+onScroll()
+document.addEventListener('scroll', throttle(onScroll, 200))
+
+function onScroll() {
+  const about = document.getElementById('about')
+  if (elementInViewport(about)) {
+    const diplomaAboutWire = document.getElementById('to-about')
+    animate([about, diplomaAboutWire])
+  }
+}
+
+function elementInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  const isInViewport = window.innerHeight - rect.top > 0
+  return isInViewport
+}
+
+function animate(elements) {
+  elements.forEach(element => element.classList.add('anim'))
+}
